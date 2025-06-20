7d5060757122f489fc0da6de4335a9eb16c0b7f4
Joel Stidley <30506169+joelst@users.noreply.github.com>
2025-06-20 10:57:23 -0500

    Update New-UmiDeployment.ps1
    Updated graph scopes and streamlined process.

--- New-UmiDeployment.ps1
+++ New-UmiDeployment.ps1
@@ -24,6 +24,16 @@ param (
   [string] $AzRegion = 'eastus'
 )
 
+# Install the required PowerShell modules
+Install-Module Az.Resources -Scope CurrentUser -SkipPublisherCheck -Force -AllowClobber -AcceptLicense
+Install-Module Microsoft.Graph.Authentication -Scope CurrentUser -SkipPublisherCheck -Force -AllowClobber -AcceptLicense
+Install-Module Microsoft.Graph.Applications -Scope CurrentUser -SkipPublisherCheck -Force -AllowClobber -AcceptLicense
+
+# Add required Azure Resource Providers
+Register-AzResourceProvider -ProviderNamespace Microsoft.Insights
+Register-AzResourceProvider -ProviderNamespace Microsoft.ManagedServices
+Register-AzResourceProvider -ProviderNamespace Microsoft.ManagedIdentity
+
 do {
   $customerPrefix = Read-Host 'Enter customer prefix name: '
 } while ($customerPrefix.Length -lt 3)
@@ -35,29 +45,14 @@ do {
 $umiName = 'MSSP-Sentinel-Ingestion-UMI'
 $rg = "$($customerPrefix.ToUpper())-Sentinel-Prod-rg"
 
-$context = Set-AzContext -SubscriptionId $Subscription
-
-# Install the required PowerShell modules
-Install-Module Az.Resources -Scope CurrentUser -SkipPublisherCheck -Force -AllowClobber -AcceptLicense
-Install-Module Microsoft.Graph.Authentication -Scope CurrentUser -SkipPublisherCheck -Force -AllowClobber -AcceptLicense
-Install-Module Microsoft.Graph.Applications -Scope CurrentUser -SkipPublisherCheck -Force -AllowClobber -AcceptLicense
-
-# Add required Azure Resource Providers
-Register-AzResourceProvider -ProviderNamespace Microsoft.Insights
-Register-AzResourceProvider -ProviderNamespace Microsoft.ManagedServices
-Register-AzResourceProvider -ProviderNamespace Microsoft.ManagedIdentity
-# Default resource group for managed identities
-
-$graphAppId = "00000003-0000-0000-c000-000000000000" # Don't change this.
-
-# Graph API permissions to set
-$addPermissions = @(
-  "Application.ReadWrite.OwnedBy"
-)
+# If running locally, instead of cloud shell, you will need to authenticate to Azure.
+# Connect-AzAccount
+Set-AzContext -SubscriptionId $Subscription
 
 # Get the context of the current subscription
 $subscriptionId = (Get-AzContext).Subscription.Id
 $scope = "/subscriptions/$($subscriptionId)"
+
 # Azure RBAC role IDs needed
 $azureOwnerRoleId = '8e3af657-a8ff-443c-a75c-2fe8c4bcb635' # This is Owner.
 $azureKVAdminRoleId = '00482a5a-887f-4fb3-b363-3b7fe8e74483' # This is Key Vault Administrator.
@@ -73,39 +68,55 @@ if ([string]::IsNullOrEmpty((Get-AzResourceGroup -Name $rg -ErrorAction Silently
 $null = New-AzUserAssignedIdentity -Name $umiName -ResourceGroupName $rg -Location $AzRegion
 $umi = Get-AzADServicePrincipal -DisplayName $umiName
 
-Connect-AzureAD -TenantId $context.Tenant.ID
-# If Connect-AzureAd does not work, run 'Connect-AzureAD -TenantId <CustomerTenantId>'
+# Wait for the UMI to be created
 Start-Sleep 10
 
 # Assign User Assigned Identity Owner permissions to the subscription
 New-AzRoleAssignment -RoleDefinitionId $azureOwnerRoleId -ObjectId $umi.Id -Scope $scope
-Start-Sleep 5
+
+# Assign Key Vault permissions to the UMI
+# The UMI needs to be able to manage Key Vaults, so we assign it the
 New-AzRoleAssignment -RoleDefinitionId $azureKVAdminRoleId -ObjectId $umi.Id -Scope $scope
-Start-Sleep 5
+
+# Assign Key Vault Secrets User permissions to the UMI
+# The UMI needs to be able to read secrets from Key Vaults, so we assign it the Key Vault Secrets User role.
+# This role allows the UMI to read secrets from Key Vaults, which is necessary for its operation.
 New-AzRoleAssignment -RoleDefinitionId $azureKVUserRoleId -ObjectId $umi.Id -Scope $scope
-Start-Sleep 5
+
+# Wait for the role assignments to propagate
+Start-Sleep 15
 
 # Create the service principal/app registration
-New-AzADServicePrincipal -DisplayName $AppName
+$appName = "MSSP-Sentinel-Ingestion"
+New-AzADServicePrincipal -DisplayName $appName
 Start-Sleep 20
-$adsp = Get-AzADServicePrincipal -DisplayName $AppName
+$adsp = Get-AzADServicePrincipal -DisplayName $appName
 
 # Assign the UMI Monitoring Metrics Publisher role to the subscription.
 # This role is required to allow the UMI to publish data to Azure Sentinel/Azure Monitor.
-
 New-AzRoleAssignment -RoleDefinitionId $metricsPubRoleId -ObjectId $adsp.Id -Scope $subscriptionId
 
-# Assign UMI permissions to read applications and manage owned applications.
-$graphSP = Get-AzADServicePrincipal -appId $graphAppId
-# Graph API permissions to set
+# The UMI needs to be granted permissions to the Microsoft Graph API
+# to allow it to view and manage its owned resources in the tenant.
+
+# Connect to Microsoft Graph. You will need to complete the authentication outside the shell.
+Connect-MgGraph -Scopes 'Application.ReadWrite.All', 'Directory.Read.All', 'AppRoleAssignment.ReadWrite.All' -NoWelcome
+
+# Get the Service Principal for Microsoft Graph
+$graphSP = Get-MgServicePrincipal -Filter "AppId eq '00000003-0000-0000-c000-000000000000'"
+
+# Graph API permissions to assign to the UMI
+# This allows the UMI to manage its service principal
 $addPermissions = @(
   'Application.ReadWrite.OwnedBy',
   'Application.Read.All'
 )
-$appRoles = $graphSP.AppRole |
-  Where-Object { ($_.Value -in $addPermissions) -and ($_.AllowedMemberType -contains 'Application') }
+
+$appRoles = $graphSP.AppRoles |
+  Where-Object { ($_.Value -in $addPermissions) -and ($_.AllowedMemberTypes -contains 'Application') }
+
 $appRoles | ForEach-Object {
-  New-AzureAdServiceAppRoleAssignment -ObjectId $umi.Id -PrincipalId $umi.Id -ResourceId $graphSp.Id -Id $_.Id
+  New-MgServicePrincipalAppRoleAssignment -ResourceId $graphSP.Id -PrincipalId $umiId -AppRoleId $_.Id -ServicePrincipalId $umiId
 }
 
 # Make sure the UMI is set as the owner of the application. This is required to allow
@@ -114,10 +125,8 @@ $newOwner = @{
   '@odata.id' = "https://graph.microsoft.com/v1.0/directoryObjects/{$($Umi.Id)}"
 }
 
-# Connect to Microsoft Graph. You will need to complete the authentication outside the shell.
-Connect-MgGraph -Scopes 'Application.ReadWrite.All', 'Directory.Read.All' -NoWelcome
-
-# This adds the UMI as an owner
+# This adds the UMI as an owner to application
 New-MgApplicationOwnerByRef -ApplicationId $Id -BodyParameter $newOwner
+
 # This will update the name to be in line with our new standard name
 Update-MgApplication -ApplicationId $Id -DisplayName 'MSSP-Sentinel-Ingestion'
